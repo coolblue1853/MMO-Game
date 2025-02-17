3375195483da095ca0084228680fcd3e72e32a1a
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2025-02-17 20:45:28 +0900

    Zone 단위로 구분한 브로드캐스팅 // A* 알고리즘 개선 // PingCheck

--- Client/Assets/Scripts/Managers/Contents/ObjectManager.cs
+++ Client/Assets/Scripts/Managers/Contents/ObjectManager.cs
@@ -6,47 +6,52 @@ using UnityEngine;
 
 public class ObjectManager
 {
-	public MyPlayerController MyPlayer { get; set; }
-	Dictionary<int, GameObject> _objects = new Dictionary<int, GameObject>();
-	
-	public static GameObjectType GetObjectTypeById(int id)
-	{
-		int type = (id >> 24) & 0x7F;
-		return (GameObjectType)type;
-	}
+    public MyPlayerController MyPlayer { get; set; }
+    Dictionary<int, GameObject> _objects = new Dictionary<int, GameObject>();
 
-	public void Add(ObjectInfo info, bool myPlayer = false)
-	{
-		GameObjectType objectType = GetObjectTypeById(info.ObjectId);
-		if (objectType == GameObjectType.Player)
-		{
-			if (myPlayer)
-			{
-				GameObject go = Managers.Resource.Instantiate("Creature/MyPlayer");
-				go.name = info.Name;
-				_objects.Add(info.ObjectId, go);
+    public static GameObjectType GetObjectTypeById(int id)
+    {
+        int type = (id >> 24) & 0x7F;
+        return (GameObjectType)type;
+    }
 
-				MyPlayer = go.GetComponent<MyPlayerController>();
-				MyPlayer.Id = info.ObjectId;
-				MyPlayer.PosInfo = info.PosInfo;
-				MyPlayer.Stat = info.StatInfo;
-				MyPlayer.SyncPos();
-			}
-			else
-			{
-				GameObject go = Managers.Resource.Instantiate("Creature/Player");
-				go.name = info.Name;
-				_objects.Add(info.ObjectId, go);
+    public void Add(ObjectInfo info, bool myPlayer = false)
+    {
+        if (MyPlayer != null && MyPlayer.Id == info.ObjectId)
+            return;
+        if (_objects.ContainsKey(info.ObjectId))
+            return;
 
-				PlayerController pc = go.GetComponent<PlayerController>();
-				pc.Id = info.ObjectId;
-				pc.PosInfo = info.PosInfo;
-				pc.Stat = info.StatInfo;
-				pc.SyncPos();
-			}
-		}
-		else if (objectType == GameObjectType.Monster)
-		{
+        GameObjectType objectType = GetObjectTypeById(info.ObjectId);
+        if (objectType == GameObjectType.Player)
+        {
+            if (myPlayer)
+            {
+                GameObject go = Managers.Resource.Instantiate("Creature/MyPlayer");
+                go.name = info.Name;
+                _objects.Add(info.ObjectId, go);
+
+                MyPlayer = go.GetComponent<MyPlayerController>();
+                MyPlayer.Id = info.ObjectId;
+                MyPlayer.PosInfo = info.PosInfo;
+                MyPlayer.Stat.MergeFrom(info.StatInfo);
+                MyPlayer.SyncPos();
+            }
+            else
+            {
+                GameObject go = Managers.Resource.Instantiate("Creature/Player");
+                go.name = info.Name;
+                _objects.Add(info.ObjectId, go);
+
+                PlayerController pc = go.GetComponent<PlayerController>();
+                pc.Id = info.ObjectId;
+                pc.PosInfo = info.PosInfo;
+                pc.Stat.MergeFrom(info.StatInfo);
+                pc.SyncPos();
+            }
+        }
+        else if (objectType == GameObjectType.Monster)
+        {
             GameObject go = Managers.Resource.Instantiate("Creature/Monster");
             go.name = info.Name;
             _objects.Add(info.ObjectId, go);
@@ -57,67 +62,72 @@ public class ObjectManager
             mc.Stat = info.StatInfo;
             mc.SyncPos();
         }
-		else if (objectType == GameObjectType.Projectile)
-		{
-			GameObject go = Managers.Resource.Instantiate("Creature/Arrow");
-			go.name = "Arrow";
-			_objects.Add(info.ObjectId, go);
+        else if (objectType == GameObjectType.Projectile)
+        {
+            GameObject go = Managers.Resource.Instantiate("Creature/Arrow");
+            go.name = "Arrow";
+            _objects.Add(info.ObjectId, go);
 
-			ArrowController ac = go.GetComponent<ArrowController>();
-			ac.PosInfo = info.PosInfo;
-			ac.Stat = info.StatInfo;
-			ac.SyncPos();
-		}
-	}
+            ArrowController ac = go.GetComponent<ArrowController>();
+            ac.PosInfo = info.PosInfo;
+            ac.Stat = info.StatInfo;
+            ac.SyncPos();
+        }
+    }
 
-	public void Remove(int id)
-	{
-		GameObject go = FindById(id);
-		if (go == null)
-			return;
+    public void Remove(int id)
+    {
+        if (MyPlayer != null && MyPlayer.Id == id)
+            return;
+        if (_objects.ContainsKey(id) == false)
+            return;
 
-		_objects.Remove(id);
-		Managers.Resource.Destroy(go);
-	}
+        GameObject go = FindById(id);
+        if (go == null)
+            return;
 
-	public GameObject FindById(int id)
-	{
-		GameObject go = null;
-		_objects.TryGetValue(id, out go);
-		return go;
-	}
+        _objects.Remove(id);
+        Managers.Resource.Destroy(go);
+    }
 
-	public GameObject FindCreature(Vector3Int cellPos)
-	{
-		foreach (GameObject obj in _objects.Values)
-		{
-			CreatureController cc = obj.GetComponent<CreatureController>();
-			if (cc == null)
-				continue;
+    public GameObject FindById(int id)
+    {
+        GameObject go = null;
+        _objects.TryGetValue(id, out go);
+        return go;
+    }
 
-			if (cc.CellPos == cellPos)
-				return obj;
-		}
+    public GameObject FindCreature(Vector3Int cellPos)
+    {
+        foreach (GameObject obj in _objects.Values)
+        {
+            CreatureController cc = obj.GetComponent<CreatureController>();
+            if (cc == null)
+                continue;
 
-		return null;
-	}
+            if (cc.CellPos == cellPos)
+                return obj;
+        }
 
-	public GameObject Find(Func<GameObject, bool> condition)
-	{
-		foreach (GameObject obj in _objects.Values)
-		{
-			if (condition.Invoke(obj))
-				return obj;
-		}
+        return null;
+    }
 
-		return null;
-	}
+    public GameObject Find(Func<GameObject, bool> condition)
+    {
+        foreach (GameObject obj in _objects.Values)
+        {
+            if (condition.Invoke(obj))
+                return obj;
+        }
 
-	public void Clear()
-	{
-		foreach (GameObject obj in _objects.Values)
-			Managers.Resource.Destroy(obj);
-		_objects.Clear();
-		MyPlayer = null;
-	}
+        return null;
+    }
+
+    public void Clear()
+    {
+        foreach (GameObject obj in _objects.Values)
+            Managers.Resource.Destroy(obj);
+        _objects.Clear();
+        MyPlayer = null;
+    }
 }
--- Server/Server/Game/Object/Monster.cs
+++ Server/Server/Game/Object/Monster.cs
@@ -104,7 +104,7 @@ namespace Server.Game
 				return;
 			}
 
-			List<Vector2Int> path = Room.Map.FindPath(CellPos, _target.CellPos, checkObjects: false);
+			List<Vector2Int> path = Room.Map.FindPath(CellPos, _target.CellPos, checkObjects: true);
 			if (path.Count < 2 || path.Count > _chaseCellDist)
 			{
 				_target = null;
@@ -133,7 +133,7 @@ namespace Server.Game
 			S_Move movePacket = new S_Move();
 			movePacket.ObjectId = Id;
 			movePacket.PosInfo = PosInfo;
-			Room.Broadcast(movePacket);
+			Room.Broadcast(CellPos, movePacket);
 		}
 
 		long _coolTick = 0;
@@ -179,7 +179,7 @@ namespace Server.Game
 				S_Skill skill = new S_Skill() { Info = new SkillInfo() };
 				skill.ObjectId = Id;
 				skill.Info.SkillId = skillData.id;
-				Room.Broadcast(skill);
+				Room.Broadcast(CellPos, skill);
 
 				// 스킬 쿨타임 적용
 				int coolTick = (int)(1000 * skillData.cooldown);
