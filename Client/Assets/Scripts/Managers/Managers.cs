36e60b309eab3ae4fe001c726127b28b12f667e6
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2025-02-19 14:19:15 +0900

    웹서버를 이용한 어카운트서버 제작

--- Client/Assets/Scripts/Managers/Managers.cs
+++ Client/Assets/Scripts/Managers/Managers.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class Managers : MonoBehaviour
 {
     static Managers s_instance; // 유일성이 보장된다
-    static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고온다
+   public static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고온다
 
     #region Contents
+    WebManager _web = new WebManager();
     InventoryManager _inven = new InventoryManager();
     MapManager _map = new MapManager();
     ObjectManager _obj = new ObjectManager();
     NetworkManager _network = new NetworkManager();
 
+    public static WebManager Web { get { return Instance._web; } }
     public static InventoryManager Inven { get { return Instance._inven; } }
     public static MapManager Map { get { return Instance._map; } }
     public static ObjectManager Object { get { return Instance._obj; } }
